4f0874f7016b207ff11e898b2e6b7ed80e3c2aeb
Dsaar <danieljoshuasaar@gmail.com>
2025-08-04 22:48:37 +0300

    api change temporary

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,4 +1,5 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = 'https://monkfish-app-z9uza.ondigitalocean.app/bcard2'
 
 const ENDPOINTS = {
 	users: {
