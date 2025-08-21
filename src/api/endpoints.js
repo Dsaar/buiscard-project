be3b32adb1912ae2d2922931998d662dff4bd0a3
Dsaar <danieljoshuasaar@gmail.com>
2025-08-21 16:52:33 +0300

    api change

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,5 +1,5 @@
 //const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const BASE_URL = 'https://monkfish-app-z9uza.ondigitalocean.app/bcard2'
+const BASE_URL = 'http://localhost:3000'
 
 const ENDPOINTS = {
 	users: {
